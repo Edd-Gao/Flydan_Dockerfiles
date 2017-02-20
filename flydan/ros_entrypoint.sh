40cb33e10e19c748da29c077d9c4e44d1db53f61
Gao Changyu <gaochangyu@163.com>
2017-02-20 14:53:15 +0800

    update

--- flydan/ros_entrypoint.sh
+++ flydan/ros_entrypoint.sh
@@ -4,4 +4,6 @@ set -e
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/catkin_ws/devel/setup.bash"
+/Firmware/integrationtests/setup_gazebo_ros.bash /Firmware
+
 exec "$@"
