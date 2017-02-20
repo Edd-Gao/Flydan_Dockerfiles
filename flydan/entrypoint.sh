5199bf3eb83a132d632527ec0f4c2e98257e680f
Gao Changyu <gaochangyu@163.com>
2017-02-20 17:00:31 +0800

    update

--- flydan/entrypoint.sh
+++ flydan/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/catkin_ws/devel/setup.bash"
-/Firmware/integrationtests/setup_gazebo_ros.bash /Firmware
+source /Firmware/integrationtests/setup_gazebo_ros.bash /Firmware
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/models
 
 exec "$@"
